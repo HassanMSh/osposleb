9561c1417283885e2cade165ead7016600dd87f4
HassanMSh <hassan.m.shamseddine@gmail.com>
2026-09-20 17:58:17 +0300

    fix: disable unsafe formatter comparisons

--- .php-cs-fixer.no-header.php
+++ .php-cs-fixer.no-header.php
@@ -31,6 +31,8 @@ $finder = Finder::create()
 $overrides = [
     // For updating to coding-standard
     'modernize_strpos'    => true,
+    'strict_comparison' => false,
+    'strict_param'      => false,
 ];
 
 $options = [
